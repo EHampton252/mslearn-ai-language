15d583710f6d7e6d0b05ce9ce724333fd4588ce2
Ethan Hampton <ethan.hampton@neudesic.com>
2025-04-23 09:29:15 -0400

    feat: completing 03 and 04

--- Labfiles/04-text-classification/C-Sharp/classify-text/Program.cs
+++ Labfiles/04-text-classification/C-Sharp/classify-text/Program.cs
@@ -3,9 +3,8 @@ using System.IO;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
-// Import namespaces
-
+using Azure;
+using Azure.AI.TextAnalytics;
 
 namespace classify_text
 {
@@ -24,6 +23,9 @@ namespace classify_text
                 string deploymentName = configuration["Deployment"];
 
                 // Create client using endpoint and key
+                AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+                Uri endpoint = new Uri(aiSvcEndpoint);
+                TextAnalyticsClient aiClient = new TextAnalyticsClient(endpoint, credentials);
 
 
                 // Read each text file in the articles folder
@@ -42,16 +44,40 @@ namespace classify_text
                 }
 
                 // Get Classifications
+                ClassifyDocumentOperation operation = await aiClient.SingleLabelClassifyAsync(WaitUntil.Completed, batchedDocuments, projectName, deploymentName);
 
+                int fileNo = 0;
+                await foreach (ClassifyDocumentResultCollection documentsInPage in operation.Value)
+                {
 
+                    foreach (ClassifyDocumentResult documentResult in documentsInPage)
+                    {
+                        Console.WriteLine(files[fileNo].Name);
+                        if (documentResult.HasError)
+                        {
+                            Console.WriteLine($"  Error!");
+                            Console.WriteLine($"  Document error code: {documentResult.Error.ErrorCode}");
+                            Console.WriteLine($"  Message: {documentResult.Error.Message}");
+                            continue;
+                        }
+
+                        Console.WriteLine($"  Predicted the following class:");
+                        Console.WriteLine();
+
+                        foreach (ClassificationCategory classification in documentResult.ClassificationCategories)
+                        {
+                            Console.WriteLine($"  Category: {classification.Category}");
+                            Console.WriteLine($"  Confidence score: {classification.ConfidenceScore}");
+                            Console.WriteLine();
+                        }
+                        fileNo++;
+                    }
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
-
-
-
     }
 }
